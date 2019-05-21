97819c87eed41b310ef7a7084a15e86bb1d93a4f
marie <marie@mariefelton.com>
2019-05-21 08:29:20 -0400

    updates to scraper script, looping through content and saving to JSON file

--- scraper.py
+++ scraper.py
@@ -1,8 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 job_titles = ['full stack developer', 'website developer', 'ux designer', 'ux developer', 'ux/ui developer', 'front end developer']
 job_city = input("City to search?")
+
+job_data = {}
 
 if not job_city:
 	exit()
@@ -10,16 +13,22 @@ else:
 
 	#loop through our job titles
 	for val in job_titles:
-		webpage_response = requests.get("https://www.indeed.ca/jobs?q=" + val + "&l=" + job_city + "&limit=30")
+		webpage_response = requests.get("https://www.indeed.ca/jobs?q=" + val + "&l=" + job_city + "&limit=2")
 		webpage = webpage_response.content
 		soup = BeautifulSoup(webpage, "html.parser")
 
 	#loop through result div and get the title
 		for card in soup.find_all("div", class_="result"):
-			cards_title = card.a.getText()
-			
-			for card in card.find_all('span', attrs={'class': 'company'}):
-				cards_text = card.getText()
+			for card in card.find_all('a', attrs={'class': 'jobtitle'}):
+				job_data['Job Title'] = card.getText().strip()
 
-				print(cards_text)
-				print(cards_title)
+		for card in soup.find_all("div", class_="result"):
+			for card in card.find_all('span', attrs={'class': 'company'}):
+				job_data['Company'] = card.getText().strip()
+
+		for card in soup.find_all("div", class_="result"):		
+			for card in card.find_all('div', attrs={'class': 'summary'}):
+				job_data['Summary'] = card.getText().strip()
+	
+				with open('joblist.txt', 'a') as outfile:  
+					json.dump(job_data, outfile, sort_keys=True, indent=4)
